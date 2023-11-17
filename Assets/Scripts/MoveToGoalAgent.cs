215ac4a8e54feb87bbc686f562c924cefbb1d05a
Carmine Napolitano <carmine-2000@outlook.it>
2023-11-17 18:24:16 +0100

    fixed issues and first test

--- Assets/Scripts/MoveToGoalAgent.cs
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -15,7 +15,7 @@ public class MoveToGoalAgent : Agent
         Debug.Log("OnEpisodeBegin!");
         // Ripristina la trasformazione dell'oggetto alla trasformazione originale
         transform.position = new Vector3(-15f,-0.15f,1.5f);
-        transform.rotation = Quaternion.identity;
+        // transform.rotation = Quaternion.identity;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -25,20 +25,22 @@ public class MoveToGoalAgent : Agent
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float move = actions.ContinuousActions[0];
-        float angle = actions.ContinuousActions[1];
+        float moveZ = actions.ContinuousActions[0];
+        float moveX = actions.ContinuousActions[1];
 
         float moveSpeed = 0.2f;
 
+        transform.position += new Vector3(moveX,0,moveZ) * Time.deltaTime * moveSpeed;
+
         // Converti l'angolo in un vettore di direzione
-        Vector3 direction = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0f, Mathf.Cos(angle * Mathf.Deg2Rad));
+        // Vector3 direction = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0f, Mathf.Cos(angle * Mathf.Deg2Rad));
 
         // Calcola il movimento in base alle azioni ricevute
-        Vector3 movement = direction * move * moveSpeed * Time.deltaTime;
+        //Vector3 movement = direction * move * moveSpeed * Time.deltaTime;
 
         // Applica il movimento all'oggetto
-        transform.Translate(movement);
-        transform.Rotate(0f, angle*0.1f, 0f);
+        // transform.Translate(movement);
+        //transform.Rotate(0f, angle*0.1f, 0f);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -48,18 +50,22 @@ public class MoveToGoalAgent : Agent
         continousActions[0] = Input.GetAxisRaw("Vertical");
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void OnCollisionEnter(Collision other)
     {
-        if (other.TryGetComponent<Wall> (out Wall wall))
+        if (other.gameObject.CompareTag("Arrivo"))
         {
             SetReward(+1f);
-            Debug.Log("Ho colpito il muro, fratellì!");
+            Debug.Log("Complimenti");
             EndEpisode();
-        } else if (!other.TryGetComponent<Plane>(out Plane plane))
+        } else if (other.gameObject.CompareTag("Birillo") || other.gameObject.CompareTag("Dosso"))
         {
             SetReward(-1f);
             Debug.Log("Ho colpito un cono o una sbarra");
             EndEpisode();
+        }
+        else if (other.gameObject.CompareTag("Percorso"))
+        {
+            SetReward(+10f);
         }
     }
 }
